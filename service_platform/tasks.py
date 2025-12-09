de29e566191bb7bd07f90353d883f44555cf6edb
RaselmamunSD <rasel.mamun314@gmail.com>
2025-12-09 22:44:30 +0600

    tasks new code add

--- service_platform/tasks.py
+++ service_platform/tasks.py
@@ -67,3 +67,32 @@ def booking_dot_com_review():
     
 
 
+@shared_task
+def tripadvisor_review():
+    print("insdie the tripadvisor")
+    PLATFORM_NAME = 'tripadvisor'
+    auth_token = settings.WEXTRACTOR_API_KEY
+
+    users = get_platform_users(PLATFORM_NAME)
+    for user in users:
+        try:
+            service_platform = get_platform(user,PLATFORM_NAME)
+            page_link = service_platform.platform_link
+            
+            page_id = extract_page_id(PLATFORM_NAME,page_link)
+            if not page_id:
+                logger.exception("Failed to fetch page id",user)
+                continue
+            
+            WEXTRACTOR_TRIPADVISOR_API = "https://wextractor.com/api/v1/reviews/tripadvisor"
+            PLATFORM_URL =  f"https://wextractor.com/api/v1/reviews/tripadvisor?id={page_id}&auth_token={auth_token}"
+            
+            fetch_and_save_reviews(WEXTRACTOR_TRIPADVISOR_API,PLATFORM_URL,service_platform,page_id)
+        except Exception as e:
+           print(f"Error to fetch data for {user},{e}")
+            
+   
+
+@shared_task
+def test():
+    return print("This is test")
