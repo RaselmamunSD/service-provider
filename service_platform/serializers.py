a276f79265c75206635041960123909916641e87
RaselmamunSD <rasel.mamun314@gmail.com>
2025-12-11 21:25:41 +0600

    new code serilaizer

--- service_platform/serializers.py
+++ service_platform/serializers.py
@@ -31,4 +31,39 @@ class ServicePlatformsCreateSerializer(serializers.ModelSerializer):
         """Custom validation to check if service_provider and platform exist before default validation."""
         errors = {}
 
-      
+        #  Validate service_provider
+        service_provider_id = data.get("service_provider")
+        if not CustomUser.objects.filter(id=service_provider_id).exists():
+            errors["service_provider"] = ["Service provider not found."]
+
+        # Validate platform
+        platform_id = data.get("platform")
+        if not Platform.objects.filter(id=platform_id).exists():
+            errors["platform"] = ["Platform not found."]
+
+        # Raise validation error if any issue found
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super().to_internal_value(data)
+    
+class CustomerListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = [
+            'name',
+            'email',
+            'phone_number',
+            'address',
+            'is_sent_email',
+            'is_given_review'
+            ]
+
+class CustomerCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['name', 'email', 'phone_number', 'address']
+    
+class CampaignSerializer(serializers.ModelSerializer):
+    
+    
